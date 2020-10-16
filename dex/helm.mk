ee3ac5335e63c7cdac076009a0ccb796ad9d1077
Jan Steffen <jan.steffen@finleap.com>
2020-10-16 13:44:50 +0200

    Add makefile for rendering/installing helm chart

---
+++ dex/helm.mk
@@ -0,0 +1,21 @@
+HELM                ?= helm3
+HELM_OUTPUT_DIR     ?= tmp
+K8S_NAMESPACE ?= dex
+
+.PHONY: helm-template-clean helm-dependency-update helm-install helm-uninstall helm-template
+
+template-clean:
+	@rm -Rf $(HELM_OUTPUT_DIR)
+
+dependency-update:
+	@$(HELM) dep update .
+
+install:
+	@$(HELM) upgrade --install dex . --namespace $(K8S_NAMESPACE)
+
+uninstall:
+	@$(HELM) uninstall dex --namespace $(K8S_NAMESPACE)
+
+template: helm-template-clean
+	@mkdir -p $(HELM_OUTPUT_DIR)
+	@$(HELM) template dex $(HELM_PATH) --namespace $(K8S_NAMESPACE) --output-dir $(HELM_OUTPUT_DIR) --include-crds
